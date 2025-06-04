715a2fe683e9ca3e6d19d4b6b4f7d9e1dbb8f16f
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2025-06-04 13:49:46 +0700

    initial commit

---
+++ frontend/next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'image.tmdb.org', // TMDB images
+      'img.omdbapi.com', // OMDB images
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  // Enable styled-components
+  compiler: {
+    styledComponents: true,
+  },
+  // Disable TypeScript
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig 
