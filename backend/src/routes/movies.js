136ed72de1b731314b14928901d0b5e336705099
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2025-06-11 16:22:50 +0700

    refactor: Remove unused PrismaClient initialization

--- backend/src/routes/movies.js
+++ backend/src/routes/movies.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
 
 // Get movie recommendations
 router.get('/recommendations', async (req, res) => {
