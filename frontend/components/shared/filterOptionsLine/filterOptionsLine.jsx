ef3f25b922e0a563e90413d3ac6dda59a65e5873
nengoilaHoang <levanviethoang04@gmail.com>
2025-06-14 01:57:45 +0700

    fix: fix filter ui in view movies page

--- frontend/components/shared/filterOptionsLine/filterOptionsLine.jsx
+++ frontend/components/shared/filterOptionsLine/filterOptionsLine.jsx
@@ -1,13 +1,68 @@
+'use client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './filterOptionsLine.css'
+import { useEffect, useState } from 'react';
+
+function FilterOption({option, isSelected, onClick, isAll}){
+    
+    // const[selectedClass, setSelectedClass] = useState('')
+    // // const[isSelectedOption, setSelectedOption] = useState(isSelected)
+    // const[isSelectedOption, setSelectedOption] = useState(isSelected)
+    
+    // useEffect(()=>{
+    //     setSelectedClass(isSelectedOption ? '-selected' : '')
+    // }, [isSelectedOption]);
+
+    // function changeSelectedOption(){    
+    //     setSelectedOption(!isSelectedOption);
+    // }
+    return(
+        <button className={`option${isSelected?"-selected":""}`} onClick={()=>onClick(option, isAll)}>{option}</button>
+    )
+}
 
 function FilterOptionsLine({title, options}){
-    return(
-        <div className="options-line d-flex flex-column w-100">
-            <div className="title-side">
-                {title}
-            </div>
-            <div className="options-side">
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [isAllSelected, setAllSelected] = useState(true)
 
+    useEffect(()=>{
+        if(selectedOptions.length===0){setAllSelected(true)}
+    },[selectedOptions])
+
+    function handleChangeSelectedClick(option, isAll){
+        if(isAll){
+            setAllSelected(true);
+            setSelectedOptions([])
+        }
+        else{
+            setAllSelected(false);
+            if(selectedOptions.includes(option)){
+                setSelectedOptions(selectedOptions.filter(item => item!== option));
+            }else{
+                setSelectedOptions([...selectedOptions,option])
+            }
+        }
+    }
+
+    return(
+        <div className="options-line d-flex flex-row w-100">
+            <div className="title-side pe-2">
+                {title}:
+            </div>
+            <div className="options-side d-flex flex-row">
+                <FilterOption 
+                    option={'tất cả'} 
+                    isSelected={isAllSelected}
+                    onClick={handleChangeSelectedClick}
+                    isAll={true}/>
+                {(Array.isArray(options) ? options : []).map((option, index)=>(
+                    <FilterOption 
+                        key={index} 
+                        option={option.option}
+                        isSelected={selectedOptions.includes(option.option)}
+                        onClick={handleChangeSelectedClick}
+                        isAll={false}/>
+                ))}
             </div>
         </div>
     ) 
--- frontend/components/shared/movieFilter/movieFilter.jsx
+++ frontend/components/shared/movieFilter/movieFilter.jsx
@@ -4,9 +4,54 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import FilterOptionsLine from '../filterOptionsLine/filterOptionsLine';
 import {useState, useEffect} from 'react'
 
+let filterOptions = [
+    {
+        title: "Quốc gia",
+        options: [
+            {option: "Việt Nam", isSelected: false},
+            {option: "Trung Quốc", isSelected: false}
+        ]
+    },
+    {
+        title: "Loại phim",
+        options: [
+            {option: "Phim lẻ", isSelected: false},
+            {option: "Phim bộ", isSelected: false}
+        ]
+    },
+    {
+        title: "Thể loại",
+        options: [
+            {option: "Anime", isSelected: false},
+            {option: "Bí ẩn", isSelected: false},
+            {option: "Chiến tranh", isSelected: false}
+        ]
+    },
+    {
+        title: "Năm sản xuất",
+        options: [
+            {option: "2025", isSelected: false},
+            {option: "2024", isSelected: false},
+            {option: "2023", isSelected: false},
+            {option: "2022", isSelected: false},
+            {option: "2021", isSelected: false},
+            {option: "2020", isSelected: false}
+        ]
+    },
+    {
+        title: "Sắp xếp theo",
+        options: [
+            {option: "Mới nhất", isSelected: false},
+            {option: "Điểm đánh giá", isSelected: false},
+            {option: "Lượt xem", isSelected: false}
+        ]
+    }
+]
+
 function MovieFilter(){
     const [isFilterHover, setFilterHover] = useState(false)
     const [isFilterShow, setFilterShow] = useState(false)
+    const [moviesFilterOptions, setmoviesFilterOptions] = useState(filterOptions)
     
     function changeFilterState(){
         if(!isFilterShow){
@@ -28,7 +73,12 @@ function MovieFilter(){
                 <h4 className="filter-text" style={{color: isFilterHover ? 'orange' : 'black'}}>filter</h4>
             </div>
             <div className="filter-options p-4" style={{display: isFilterShow ? 'flex':'none'}}>
-                <FilterOptionsLine title={"Quốc gia:"}/>
+                <div className='d-flex flex-column'>
+                    {filterOptions.map((optionsLine, index)=>(
+                        <FilterOptionsLine key={index} title={optionsLine.title} options={optionsLine.options}/>
+                    ))}
+                    <button className='filter-button'>Lọc kết quả</button>
+                </div>
             </div>
         </div>
     )
